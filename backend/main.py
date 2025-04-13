3fa12c957e60f69ad37f42d011159af6c4c503c9
Rakib <bsse1320@iit.du.ac.bd>
2025-04-13 16:54:46 +0600

    CSV changed

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware #Cross-Origin Resource Sharing.
 import pandas as pd
 
 app = FastAPI()
