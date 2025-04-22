cafa16ce9bc0346b92ab02857fcf83de9bc200e6
fareya22 <bsse1331@iit.du.ac.bd>
2025-04-22 12:26:19 +0600

    map moved to app

---
+++ frontend/app/components/Tooltip.js
@@ -0,0 +1,17 @@
+// export default function Tooltip({ content }) {
+//   return (
+//     <div style={{
+//       position: "absolute",
+//       top: "10px",
+//       left: "10px",
+//       background: "rgba(0, 0, 0, 0.7)",
+//       color: "#FFF",
+//       padding: "5px 10px",
+//       borderRadius: "4px",
+//       pointerEvents: "none"
+//     }}>
+//       <strong>{content.name}</strong>
+//       <div>Total Policies: {content.total}</div>
+//     </div>
+//   )
+// }
