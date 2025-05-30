f2546653f3f21e5fe2e4b244218b74d86ec0ae6e
Rakib <bsse1320@iit.du.ac.bd>
2025-05-31 02:27:19 +0600

    up

--- frontend/app/admin/AdminDashboard.js
+++ frontend/app/admin/AdminDashboard.js
@@ -3,7 +3,7 @@ import './admin-dashboard.css'
 
 // API base URL
 //update env when live the server
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://policy-tracker-5.onrender.com/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://policy-tracker-5.onrender.com/';
 
 
 export default function AdminDashboard() {
